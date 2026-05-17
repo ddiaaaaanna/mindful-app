46e7661e36176174efb653ec1a08fa0aae568752
ddiaaaaanna <dpuraite@yahoo.com>
2026-05-17 14:35:58 +0300

    feat: meditation page UI

--- src/pages/Meditation/Meditation.tsx
+++ src/pages/Meditation/Meditation.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./Meditation.css";
 
 type MeditationProps = {
@@ -5,34 +6,66 @@ type MeditationProps = {
   emoji: string;
 };
 
+type Duration = {
+  label: string;
+  value: number;
+};
+
 function Meditation({ setActivePage, emoji }: MeditationProps) {
+  const [meditationTimer, setMeditationTimer] = useState<number>(0);
+  const [meditationStep, setMeditationStep] = useState<string>("choose");
+
+  const durations: Duration[] = [
+    { label: "1 min", value: 60 },
+    { label: "2 min", value: 120 },
+    { label: "3 min", value: 180 },
+  ];
+
+  console.log(meditationTimer);
+
   return (
-    <div className="meditation-page">
-      <div className="meditation-header flex-center">
-        <h1>MEDITATION</h1>
-        <button className="home-btn" onClick={() => setActivePage("")}>
-          Home
-        </button>
-      </div>
+    <>
+      {meditationStep === "choose" && (
+        <>
+          <div className="meditation-page">
+            <div className="meditation-header flex-center">
+              <h1>MEDITATION</h1>
+              <button className="home-btn" onClick={() => setActivePage("")}>
+                Home
+              </button>
+            </div>
 
-      <div className="meditation-page-content">
-        <p className="meditation-card-description">
-          Choose the duration of your practice
-        </p>
-        <div className="choice-btn-container flex-center">
-          <button className="meditation-choice-btn">1 min</button>
-          <button className="meditation-choice-btn">2 min</button>
-          <button className="meditation-choice-btn">3 min</button>
-        </div>
+            <div className="meditation-page-content">
+              <p className="meditation-card-description">
+                Choose the duration of your practice
+              </p>
+              <div className="choice-btn-container flex-center">
+                {durations.map((duration) => (
+                  <button
+                    className={`meditation-choice-btn ${meditationTimer === duration.value ? "selected" : ""}`}
+                    onClick={() => setMeditationTimer(duration.value)}
+                    key={duration.value}
+                  >
+                    {duration.label}
+                  </button>
+                ))}
+              </div>
 
-        <p
-          className="flex-center card-emoji
-        "
-        >
-          {emoji}
-        </p>
-      </div>
-    </div>
+              <p className="flex-center card-emoji">{emoji}</p>
+
+              {meditationTimer > 0 && (
+                <button
+                  className="action-btn"
+                  onClick={() => setMeditationStep("timer")}
+                >
+                  Begin
+                </button>
+              )}
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
