83db6fac55fd2b4b114ff7518095c6d753a2dd00
ddiaaaaanna <dpuraite@yahoo.com>
2026-06-03 21:20:54 +0300

    fix: remove unused import causing build failure

--- src/pages/Exploration/Exploration.tsx
+++ src/pages/Exploration/Exploration.tsx
@@ -1,17 +1,17 @@
-// import "./Exploration.css";
+import "./Exploration.css";
 // import { useContext } from "react";
 // import { AppContext } from "../../context/AppContext";
-// import Page from "../../components/Page/Page";
+import Page from "../../components/Page/Page";
 
-// function Exploration() {
-//   const context = useContext(AppContext);
-//   if (!context) return null;
-//   const { setActivePage } = context;
-//   return (
-//     <>
-//       <Page title="EXPLORE" description="Thought of the day"></Page>
-//     </>
-//   );
-// }
+function Exploration() {
+  // const context = useContext(AppContext);
+  // if (!context) return null;
+  // const { setActivePage } = context;
+  return (
+    <>
+      <Page title="EXPLORE" description="Thought of the day"></Page>
+    </>
+  );
+}
 
-// export default Exploration;
+export default Exploration;
