292844634b258f62cfad6ad5905cb5c7ee339938
ddiaaaaanna <dpuraite@yahoo.com>
2026-06-11 11:46:38 +0300

    feat: add daily quote to explore page

--- src/pages/Exploration/Exploration.tsx
+++ src/pages/Exploration/Exploration.tsx
@@ -1,15 +1,29 @@
 import "./Exploration.css";
+import quotes from "../../data/quotes.json";
 // import { useContext } from "react";
 // import { AppContext } from "../../context/AppContext";
 import Page from "../../components/Page/Page";
 
 function Exploration() {
-  // const context = useContext(AppContext);
-  // if (!context) return null;
-  // const { setActivePage } = context;
+  const today = new Date();
+  const year = today.getFullYear();
+  const startOfYear = new Date(year, 0, 0);
+  const diff = Number(today) - Number(startOfYear);
+  const MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+  const dayOfYear = Math.floor(diff / MS_PER_DAY);
+  const todaysQuote = quotes[dayOfYear % quotes.length];
+
   return (
     <>
-      <Page title="EXPLORE" description="Thought of the day"></Page>
+      <Page title="EXPLORE" description="Thought of the day">
+        <div className="quote-container">
+          <p className="quote-author">{todaysQuote.author}</p>
+          <p className="quote-text">{todaysQuote.quote}</p>
+        </div>
+
+        <button className="action-btn">Add to journal</button>
+      </Page>
     </>
   );
 }
