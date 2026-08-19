a4fff907728a34cfddc4b4eefa0f62c1f00bf524
ddiaaaaanna <dpuraite@yahoo.com>
2026-08-19 08:18:10 +0300

    feat: add month navigation to calendar

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./Calendar.css";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardArrowLeft } from "react-icons/md";
 
 function Calendar() {
+  const [displayedDate, setDisplayedDate] = useState(new Date());
   const today = new Date();
-  const year = today.getFullYear();
-  const month = today.getMonth();
-  const monthName = today.toLocaleDateString("default", { month: "long" });
+  const yearToday = today.getFullYear();
+  const monthToday = today.getMonth();
+  const year = displayedDate.getFullYear();
+  const month = displayedDate.getMonth();
+  const monthName = displayedDate.toLocaleDateString("default", {
+    month: "long",
+  });
   const daysInMonth = new Date(year, month + 1, 0).getDate();
   const startDay = new Date(year, month, 1).getDay();
   const days = Array.from({ length: daysInMonth }, (_, i) => i + 1);
@@ -15,12 +23,34 @@ function Calendar() {
 
   const activeDays = JSON.parse(localStorage.getItem("activeDays") ?? "[]");
 
+  function nextMonth() {
+    setDisplayedDate(new Date(year, month + 1, 1));
+  }
+
+  function prevMonth() {
+    setDisplayedDate(new Date(year, month - 1, 1));
+  }
+
   return (
     <div className="calendar">
       <p className="description">Active days</p>
       <div className="calendar-container">
         <div className="calendar-header">
-          <p className="description">{monthName}</p>
+          <div className="calendar-controls">
+            <button className="" onClick={prevMonth}>
+              <MdKeyboardArrowLeft />
+            </button>
+            <p className="description">{monthName}</p>
+
+            <button
+              className={
+                year === yearToday && month === monthToday ? "unavailable" : ""
+              }
+              onClick={nextMonth}
+            >
+              <MdKeyboardArrowRight />
+            </button>
+          </div>
         </div>
         <div className="calendar-content">
           {weekdays.map((day) => (
